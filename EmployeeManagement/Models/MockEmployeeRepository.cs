4d56b8341e205a0b9cad6e0de4c301228a336d26
Ashish Guhe <ashguhe@gmail.com>
2020-03-22 20:03:23 -0400

    Video 40 : Added create view with bootstrap

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -12,9 +12,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee(){ Id = 1, Name = "Ashish", Email = "abcd@bcde.com", Department = "IT"},
-                new Employee(){ Id = 2, Name = "Pradnya", Email = "abcde@bcde.com", Department = "HR"},
-                new Employee(){ Id = 3, Name = "Monu", Email = "abcdef@bcde.com", Department = "IT"}
+                new Employee(){ Id = 1, Name = "Ashish", Email = "abcd@bcde.com", Department = DeptEnum.IT},
+                new Employee(){ Id = 2, Name = "Pradnya", Email = "abcde@bcde.com", Department = DeptEnum.HR},
+                new Employee(){ Id = 3, Name = "Monu", Email = "abcdef@bcde.com", Department = DeptEnum.IT}
             };
         }
         public Employee GetEmployee(int Id)
