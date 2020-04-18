af79c3be585c932f43ad215075f78101bd3acd26
Ashish Guhe <ashguhe@gmail.com>
2020-04-18 18:35:35 -0400

    Video 47 : Added DBConetext file

---
+++ EmployeeManagement/Models/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
