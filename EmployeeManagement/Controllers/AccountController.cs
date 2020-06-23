2097036054161c6125fcc6034429165c653ae73e
Ashish Guhe <ashguhe@gmail.com>
2020-06-22 21:27:14 -0400

    Video 70: Added Login vieew

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -62,5 +62,28 @@ namespace EmployeeManagement.Controllers
 
             return RedirectToAction("index", "home");
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError("", "Invalid login attempt!");
+            }
+            return View(model);
+        }
     }
 }
